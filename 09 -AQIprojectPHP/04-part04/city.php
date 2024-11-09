bb64947441a52c33fc4eea9664064a38fd930b59
Osama-Elwan <osama.elwan01@gmail.com>
2024-11-09 21:55:37 +0200

    start with chart.js

---
+++ 09 -AQIprojectPHP/04-part04/city.php
@@ -0,0 +1,112 @@
+
+<?php 
+include_once __DIR__. '/inc/functions.inc.php';
+$city = null;
+if(!empty($_GET['city'])){
+    
+    $city = $_GET['city'];
+}
+// var_dump($city);
+
+$filename = null;
+if(!empty($city)){
+    $cities = json_decode(
+        file_get_contents(__DIR__ . '/../data/index.json' ),
+        true
+    );
+    // var_dump($cities);
+    foreach($cities AS $currentCity) {
+        if($currentCity['city'] === $city){
+            $filename = $currentCity['filename'];
+            break;
+        }
+    }
+}
+// var_dump($filename);
+if(!empty($filename)){
+    $results = json_decode(
+        file_get_contents('compress.bzip2://' . __DIR__ . '/../data/' . $filename),
+        true
+    )['results'];
+    // var_dump($results);
+
+    $stats = [];
+    foreach ($results as $result) {
+        if($result['parameter']!== 'pm25' && $result['parameter']!== 'pm10') continue;
+        if($result['value'] < 0) continue;
+        $month = substr($result['date']['local'],0,7);
+        if(!isset($stats[$month])) {
+            $stats[$month] = [
+                'pm25' =>[],
+                'pm10' =>[],
+            ];
+        }
+            $stats[$month][$result['parameter']][]= $result['value'];
+        
+        // var_dump($month);
+        // var_dump($result);
+        
+        // break;
+    }
+    
+    //  var_dump($stats);
+}
+?>
+
+
+<?php 
+include_once __DIR__. '/views/header.inc.php';
+?>
+
+<?php if(empty($city)): ?>
+    <p>The city not be loaded.</p>
+<?php else: ?>
+    <?php if(!empty($stats)): ?>
+        <table>
+            <thead> 
+                    <tr>
+                        <th>Month</th>
+                        <th>PM 2.5 concentrarion</th>
+                        <th>PM 10 concentrarion</th>
+                        
+                    </tr>
+            </thead>
+            <tbody>
+
+            <?php foreach($stats AS $month=> $measurements) :?>
+                <tr>
+                    <th>
+                        <?php echo e($month) ?>
+                    </th>
+                    <td>
+                        <?php echo e(array_sum($measurements['pm25']) / count($measurements['pm25'])) ?>
+                    </td>
+                    <td>
+                    <?php echo e(array_sum($measurements['pm10']) / count($measurements['pm10'])) ?>
+
+                    </td>
+                </tr>
+            <?php endforeach; ?>  
+            </tbody>
+
+        </table>
+
+    <?php endif; ?>
+
+<?php endif; ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php 
+include_once __DIR__. '/views/footer.inc.php';
+?>
